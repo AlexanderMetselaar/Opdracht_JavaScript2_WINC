cfb59a9d4b8c93318114d0bfc5e660326cadcfc6
AlexanderMetselaar <80575076+AlexanderMetselaar@users.noreply.github.com>
2021-06-09 14:17:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const age = 47;
+const isFemale = false;
+const driverStatus = "Bob";
+
+if (age >= 18) {
+
+    console.log("Je bent oud genoeg om naar binnen te mogen.");
+} else {
+
+    console.log("Je bent nog niet oud genoeg om binnen gelaten te worden!")
+}
+
+if (isFemale) {
+
+    console.log("Het is LadiesNight!")
+
+} else {
+
+    console.log("Helaas, de mannen betalen wel entree!");
+}
+
+if(driverStatus == "Bob") {
+
+    console.log("Jij bent de chauffeur vanavond!")
+} else {
+
+    console.log("Ik vermoed dat jij gedronken hebt. Jij mag geen auto meer rijden!")
+}
